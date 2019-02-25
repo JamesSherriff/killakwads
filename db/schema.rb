52c7d7052ecfdb76fcde5b090da26c2524e38478
jshe10 <jamessherriff@warwickshire.gov.uk>
2019-02-25 19:06:39 +0000

    Fixes merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -97,6 +97,9 @@ ActiveRecord::Schema.define(version: 2019_02_25_152939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "profile_picture_url"
+    t.text "description"
+    t.text "location"
+    t.text "display_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
