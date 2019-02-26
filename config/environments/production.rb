e8e6d6d3e0e0639769d2c952bbf00c46f624aaf0
jshe10 <jamessherriff@warwickshire.gov.uk>
2019-02-26 21:15:38 +0000

    Switch image upload to use Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -93,7 +93,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.require_master_key = true
-
-  # Store files locally.
-  config.active_storage.service = :local
 end
