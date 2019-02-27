e174942d936b52ba9be330f1c3499a67ffd7504a
jshe10 <jamessherriff@warwickshire.gov.uk>
2019-02-27 16:00:28 +0000

    Update AWS and Sendgrid details

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'XyreJamesyoui',
-  :password => 'Icyredn0serules',
-  :domain => 'yourdomain.com',
+  :user_name => Rails.application.credentials.sendgrid_username,
+  :password => Rails.application.credentials.sendgrid_password,
+  :domain => 'killakwads.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
